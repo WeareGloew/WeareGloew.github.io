91878c95551e95d0b3e7bfb831e3285d07152a99
WeareGloew <weare.gloew@gmail.com>
2025-12-10 23:40:49 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Set year in footer
+document.addEventListener("DOMContentLoaded", () => {
+  const yearEl = document.getElementById("year");
+  if (yearEl) {
+    yearEl.textContent = new Date().getFullYear();
+  }
+});
